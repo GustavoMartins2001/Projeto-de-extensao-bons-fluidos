995c62cf2cb139ac218bee656f3ab4e056481560
GustavoMartins2001 <gustavoms050@gmail.com>
2024-11-25 19:04:24 -0300

    Criado pagina de login

--- src/app/componentes/login/login.component.ts
+++ src/app/componentes/login/login.component.ts
@@ -1,11 +1,36 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
+  constructor(private router: Router) {}
+
+  //verifica os inputs para mudar a cor do botao, pode ser usado para fazer validação no backend
+  user = {
+    username: '',
+    password: '',
+  };
+
+  onSubmit() {
+    if (this.user.username && this.user.password) {
+      console.log('Form submitted', this.user);
+
+      //logica de validação
+      //
+      //
+
+      this.router.navigate(['/listagem/evento']);
+      
+    } else {
+      console.error('Form is invalid');
+    }
+  }
 }
