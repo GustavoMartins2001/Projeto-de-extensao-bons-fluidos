e8f17e5857b14d477031f081174b94a8a503c90d
GustavoMartins2001 <gustavoms050@gmail.com>
2024-11-29 20:59:27 -0300

    criado estilização da pagina

--- src/app/componentes/cadastros/cadastro-usuario/cadastro-usuario.component.ts
+++ src/app/componentes/cadastros/cadastro-usuario/cadastro-usuario.component.ts
@@ -1,9 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-cadastro-usuario',
+  imports: [FormsModule, CommonModule],
   templateUrl: './cadastro-usuario.component.html',
   styleUrl: './cadastro-usuario.component.css'
 })
 export class CadastroUsuarioComponent {
+  constructor(private router: Router) {}
+
+  //verifica os inputs para mudar a cor do botao, pode ser usado para fazer validação no backend
+  user = {
+    username: '',
+    password: '',
+  };
+
+  onSubmit() {
+    if (this.user.username && this.user.password) {
+      console.log('Form submitted', this.user);
+
+      //logica de validação
+      //
+      //
+
+      this.router.navigate(['/listagem/evento']);
+      
+    } else {
+      console.error('Form is invalid');
+    }
+  }
 }
