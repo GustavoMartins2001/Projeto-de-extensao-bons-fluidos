8eeb363db5e06d6d317e8ef6aa9085a1ebbd3c30
ItaloF532 <italo.francisco532@gmail.com>
2024-12-03 09:12:01 -0300

    feat: integrate login page with auth service

--- src/app/componentes/login/login.component.ts
+++ src/app/componentes/login/login.component.ts
@@ -1,36 +1,54 @@
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
-import { CommonModule } from '@angular/common';
+import { AuthService } from '../../services/auth.service';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { CommonModule, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [FormsModule, CommonModule],
+  styleUrl: './login.component.css',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  standalone: true,
+  imports: [NgIf, FormsModule, CommonModule, ReactiveFormsModule],
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  form?: FormGroup;
+  loginError: boolean = false;
 
-  constructor(private router: Router) {}
+  constructor(
+    private readonly router: Router,
+    private readonly authService: AuthService
+  ) {}
 
-  //verifica os inputs para mudar a cor do botao, pode ser usado para fazer validação no backend
-  user = {
-    username: '',
-    password: '',
-  };
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      username: new FormControl<string>('', [Validators.required]),
+      password: new FormControl<string>('', [
+        Validators.required,
+        Validators.minLength(8),
+      ]),
+    });
+  }
 
-  onSubmit() {
-    if (this.user.username && this.user.password) {
-      console.log('Form submitted', this.user);
+  async onSubmit(): Promise<void> {
+    this.form?.markAllAsTouched();
 
-      //logica de validação
-      //
-      //
+    if (this.form?.invalid) {
+      this.loginError = true;
+    }
 
+    try {
+      await this.authService.login(this.form?.getRawValue());
+    } catch (_) {
+      this.loginError = true;
+    } finally {
       this.router.navigate(['/listagem/evento']);
-      
-    } else {
-      console.error('Form is invalid');
     }
   }
 }
