c483d45090d09e3fad353c66051f56460210398e
ItaloF532 <italo.francisco532@gmail.com>
2024-12-04 22:10:06 -0300

    feat: create user service

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -3,8 +3,16 @@ import { HttpClient } from '@angular/common/http';
 import { lastValueFrom } from 'rxjs';
 import { ENVIRONMENTS } from '../constants/environments';
 
+export type UserRegisterParam = {
+  name: string;
+  email: string;
+  password: string;
+  phone: string;
+  apoiador: boolean;
+};
+
 @Injectable({ providedIn: 'root', deps: [HttpClient] })
-export class AuthService {
+export class UserService {
   constructor(private readonly http: HttpClient) {}
 
   private $token: string = '';
@@ -13,17 +21,20 @@ export class AuthService {
     return this.$token;
   }
 
-  async login({
+  async register({
+    name,
     email,
+    phone,
     password,
-  }: {
-    email: string;
-    password: string;
-  }): Promise<void> {
+    apoiador,
+  }: UserRegisterParam): Promise<void> {
     this.$token = await lastValueFrom(
-      this.http.post<string>(`${ENVIRONMENTS.API_URL}/api/auth/login`, {
+      this.http.post<string>(`${ENVIRONMENTS.API_URL}/api/user/register`, {
+        name,
         email,
+        phone,
         password,
+        apoiador,
       })
     );
   }
